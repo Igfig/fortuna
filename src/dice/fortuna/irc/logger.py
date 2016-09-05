cb013eec170a629f939b3552c491f694e9c83594
Ira Fich <ira.fich@gmail.com>
2016-09-05 13:31:04 -0400

    moved config files to program root implemented RECALL from any chat

--- src/dice/fortuna/irc/logger.py
+++ src/dice/fortuna/irc/logger.py
@@ -16,15 +16,16 @@ class Logger(object):
 	formats and logs all lines from chat
 	"""
 
-	def __init__(self, queue_in, location="../../../logs/"):
+	def __init__(self, queue_in, location="../../../../logs/"):
 		self.queue_in = queue_in
-		self.location = location.rstrip('/') + "/"
+		self.location = location.rstrip('/') + '/'	# make sure there's exactly one
+													# trailing slash
 	
 	def get_logpath(self, channel):
 		return self.location + channel.lstrip('#') + "_" + \
 				str(datetime.date.today()) + ".txt"
 	
-	def log(self, msg):
+	def log(self, msg):		
 		with open(self.get_logpath(msg.context), 'a') as logfile:
 			try:
 				logfile.write(str(msg) + "\n")
@@ -38,12 +39,17 @@ class Logger(object):
 				traceback.print_tb(err.__traceback__)
 	
 	def recall(self, context, num_lines=1):
-		#TODO: maybe change the default so that it just tries to repeat everything since you logged out?
-		#TODO: be able to request the history from a different convo, so you can ask in PMs 
+		# TODO: maybe change the default so that it just tries to repeat 
+		# everything since you logged out?
 		with open(self.get_logpath(context), 'r') as logfile:
 			return [line.strip() for line in 
-				logfile.readlines()[-num_lines-1:-1]] 
-			#the -1s are so we don't recall the RECALL command 
+				logfile.readlines()[-num_lines:]] 
+				# yes, if you recall the chat you're in then you'll get back 
+				# the RECALL command you just used. It's annoying, but if I
+				# didn't do it like that then you'd lose the last line, which 
+				# ISN'T expendable, when you recall from a different chat.
+				# TODO I suppose I could have it give the last line or not 
+				# depending on if it's the same chat...
 	
 	def start(self):
 		while True:
